8f7ff528a058fe25772e51efe77d13457f85144b
greenpowerninja2 <118147535+greenpowerninja2@users.noreply.github.com>
2024-06-08 12:43:47 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function tp1(){
+window.location = "alexa.html";
+}
+function tp2(){
+    window.location = "bottle.html";
+    }
+    function tp3(){
+        window.location = "cup.html";
+        }
+        function tp4(){
+            window.location = "phone.html";
+            }
+            function tp5(){
+                window.location = "tissue.html";
+                }
